9f507929e1c7fb48ed9a017c7906c8c938b1ebee
AnnaMastalerczuk <anmastalerczuk@gmail.com>
2023-01-24 13:14:23 +0100

    model start

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,12 @@ module com.amastalerczuk {
     opens com.amastalerczuk to javafx.fxml;
     opens com.amastalerczuk.controller to javafx.fxml;
     opens com.amastalerczuk.view to javafx.fxml;
+    opens com.amastalerczuk.model;
+    opens com.amastalerczuk.model.client;
 
     exports com.amastalerczuk;
     exports com.amastalerczuk.controller;
     exports com.amastalerczuk.view;
+    exports com.amastalerczuk.model;
+    exports com.amastalerczuk.model.client;
 }
