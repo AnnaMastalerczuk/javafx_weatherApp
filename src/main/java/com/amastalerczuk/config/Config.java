1995cc8f802d83cfb3f37168103b19d566167165
AnnaMastalerczuk <anmastalerczuk@gmail.com>
2023-02-16 21:08:46 +0100

    gitignore

--- src/main/java/com/amastalerczuk/config/Config.java
+++
@@ -1,7 +0,0 @@
-package com.amastalerczuk.config;
-
-public class Config {
-    public final static String CITY_LIST_WITH_DATA = "D:\\Ania\\Z dysku H\\KURS PROGRAMOWANIA\\JAVAFX\\javafx_weatherApp\\src\\main\\resources\\com\\amastalerczuk\\cities\\city.list.min.json";
-    public final static String API_KEY = "a98684e57e4bc16b5f34ff523426fbff";
-    public final static String WEATHERAPI_KEY = "c19582afd2404466989181553231602";
-}
